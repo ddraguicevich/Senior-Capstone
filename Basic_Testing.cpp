a43f00e3dbf44efa93458165ac5cf91f8735f716
ddraguicevich <68033803+ddraguicevich@users.noreply.github.com>
2022-03-04 08:55:55 -0700

    Update Basic_Testing.cpp

--- Basic_Testing.cpp
+++ Basic_Testing.cpp
@@ -1,3 +1,11 @@
+/*
+    Basic_Testing
+    Diego Draguicevich
+    The purpose of this file is to test the basic functionality
+    of the Teensy 4.1 and our chosen DAC by outputting a steady
+    sine wave which has user-defined volume via a potentiometer
+*/
+
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -6,21 +14,23 @@
 #include <ADC.h>
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform       oscillator1;      
-AudioOutputI2S           dac;           
+AudioSynthWaveform       oscillator1;
+AudioOutputI2S           dac;
 AudioConnection          patchCord1(oscillator1, 0, dac, 0);
 AudioConnection          patchCord2(oscillator1, 0, dac, 1);
 // GUItool: end automatically generated code
 
-int main(void)
+#define VOLUME_PIN 40
+double volume = 0.5;
+double freq = 440;
+
+void setup()
 {
-    double volume = 0.5;
-    
-    oscillator1.begin(WAVEFORM_SINE);
-    while(1)
-    {
-        volume = analogRead(40) / 1024.0;
-        oscillator1.amplitude(volume);
-    }
-    return 0;
+    oscillator1.begin(volume, freq, WAVEFORM_SINE);
+}
+
+void loop()
+{
+    volume = analogRead(VOLUME_PIN) / 1024.0;
+    oscillator1.amplitude(volume);
 }
