64422322a382c5fbf6d6a2c45e2501eb6e3cfe2a
ddraguicevich <68033803+ddraguicevich@users.noreply.github.com>
2022-03-23 10:46:54 -0700

    Update Minimum_Viable_Product.cpp
    Got polyphony working!

--- Minimum_Viable_Product.cpp
+++ Minimum_Viable_Product.cpp
@@ -18,6 +18,23 @@
 
 // GUItool: begin automatically generated code
 AudioSynthWaveform       oscillator1;
+AudioSynthWaveform       oscillator2;
+AudioSynthWaveform       oscillator3;
+AudioSynthWaveform       oscillator4;
+AudioSynthWaveform       oscillator5;
+AudioSynthWaveform       oscillator6;
+AudioSynthWaveform       oscillator7;
+AudioSynthWaveform       oscillator8;
+AudioSynthWaveform       oscillator9;
+AudioSynthWaveform       oscillator10;
+AudioSynthWaveform       oscillator11;
+AudioSynthWaveform       oscillator12;
+AudioSynthWaveform       oscillator13;
+AudioSynthWaveform       oscillator14;
+AudioSynthWaveform       oscillator15;
+AudioSynthWaveform       oscillator16;
+AudioSynthWaveform       oscillator17;
+AudioSynthWaveform       oscillator18;
 AudioEffectEnvelope      envelope1;
 AudioEffectEnvelope      envelope2;
 AudioEffectEnvelope      envelope3;
@@ -26,32 +43,54 @@ AudioEffectEnvelope      envelope5;
 AudioEffectEnvelope      envelope6;
 AudioMixer4              mix1;
 AudioMixer4              mix2;
+AudioMixer4              mix3;
+AudioMixer4              mix4;
+AudioMixer4              mix5;
+AudioMixer4              mix6;
+AudioMixer4              mix7;
+AudioMixer4              mix8;
 AudioAmplifier           amp1;
 AudioOutputPT8211        dac;
-AudioConnection          patchCord1(oscillator1, 0, envelope1, 0);
-AudioConnection          patchCord2(oscillator1, 0, envelope2, 0);
-AudioConnection          patchCord3(oscillator1, 0, envelope3, 0);
-AudioConnection          patchCord4(oscillator1, 0, envelope4, 0);
-AudioConnection          patchCord5(oscillator1, 0, envelope5, 0);
-AudioConnection          patchCord6(oscillator1, 0, envelope6, 0);
-AudioConnection          patchCord7(envelope1, 0, mix1, 0);
-AudioConnection          patchCord8(envelope2, 0, mix1, 1);
-AudioConnection          patchCord9(envelope3, 0, mix1, 2);
-AudioConnection          patchCord10(envelope4, 0, mix2, 0);
-AudioConnection          patchCord11(envelope5, 0, mix2, 1);
-AudioConnection          patchCord12(envelope6, 0, mix2, 2);
-AudioConnection          patchCord13(mix1, 0, mix2, 3);
-AudioConnection          patchCord14(mix2, 0, amp1, 0);
-AudioConnection          patchCord15(amp1, 0, dac, 0);
-AudioConnection          patchCord16(amp1, 0, dac, 1);
+AudioConnection          patchCord1(oscillator1, 0, mix1, 0);
+AudioConnection          patchCord2(oscillator2, 0, mix1, 1);
+AudioConnection          patchCord3(oscillator3, 0, mix1, 2);
+AudioConnection          patchCord4(oscillator4, 0, mix2, 0);
+AudioConnection          patchCord5(oscillator5, 0, mix2, 1);
+AudioConnection          patchCord6(oscillator6, 0, mix2, 2);
+AudioConnection          patchCord7(oscillator7, 0, mix3, 0);
+AudioConnection          patchCord8(oscillator8, 0, mix3, 1);
+AudioConnection          patchCord9(oscillator9, 0, mix3, 2);
+AudioConnection          patchCord10(oscillator10, 0, mix4, 0);
+AudioConnection          patchCord11(oscillator11, 0, mix4, 1);
+AudioConnection          patchCord12(oscillator12, 0, mix4, 2);
+AudioConnection          patchCord13(oscillator13, 0, mix5, 0);
+AudioConnection          patchCord14(oscillator14, 0, mix5, 1);
+AudioConnection          patchCord15(oscillator15, 0, mix5, 2);
+AudioConnection          patchCord16(oscillator16, 0, mix6, 0);
+AudioConnection          patchCord17(oscillator17, 0, mix6, 1);
+AudioConnection          patchCord18(oscillator18, 0, mix6, 2);
+AudioConnection          patchCord19(mix1, 0, envelope1, 0);
+AudioConnection          patchCord20(mix2, 0, envelope2, 0);
+AudioConnection          patchCord21(mix3, 0, envelope3, 0);
+AudioConnection          patchCord22(mix4, 0, envelope4, 0);
+AudioConnection          patchCord23(mix5, 0, envelope5, 0);
+AudioConnection          patchCord24(mix6, 0, envelope6, 0);
+AudioConnection          patchCord25(envelope1, 0, mix7, 0);
+AudioConnection          patchCord26(envelope2, 0, mix7, 1);
+AudioConnection          patchCord27(envelope3, 0, mix7, 2);
+AudioConnection          patchCord28(envelope4, 0, mix8, 0);
+AudioConnection          patchCord29(envelope5, 0, mix8, 1);
+AudioConnection          patchCord30(envelope6, 0, mix8, 2);
+AudioConnection          patchCord31(mix7, 0, mix8, 3);
+AudioConnection          patchCord32(mix8, 0, amp1, 0);
+AudioConnection          patchCord33(amp1, 0, dac, 0);
+AudioConnection          patchCord34(amp1, 0, dac, 1);
 // GUItool: end automatically generated code
 
-double freq = 440;
-double attack, release;
+double attack, decay, sustain, release;
 bool note_on = false;
 bool new_note = false;
 int type, note, velocity, channel, d1, d2;
-std::vector<AudioEffectEnvelope> envelopes;
 
 unsigned int potentiometers[]
 {
@@ -74,10 +113,25 @@ double notes[]
   1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53
 };
 
-//Setting up for easier polyphony
-AudioEffectEnvelope * envelope_used[72];
-
-bool playing [72];
+//Setting up for polyphony
+AudioEffectEnvelope * envelopes[6]
+{
+  &envelope1, &envelope2, &envelope3, &envelope4, &envelope5, &envelope6
+};
+AudioSynthWaveform * oscillators[18]
+{
+  &oscillator1, &oscillator2, &oscillator3,
+  &oscillator4, &oscillator5, &oscillator6,
+  &oscillator7, &oscillator8, &oscillator9,
+  &oscillator10, &oscillator11, &oscillator12,
+  &oscillator13, &oscillator14, &oscillator15,
+  &oscillator16, &oscillator17, &oscillator18
+};
+bool available [6]
+{
+  true, true, true, true, true, true
+};
+int playing [72];
 
 //Playing notes with polyphony
 void play_note(const unsigned int note);
@@ -88,19 +142,12 @@ void setup()
   AudioMemory(18);
   Serial.begin(9600);
   amp1.gain(1.0);
-  oscillator1.begin(WAVEFORM_TRIANGLE);
-  oscillator1.frequency(freq);
-  oscillator1.amplitude(1.0);
-  envelopes.push_back(envelope1);
-  envelopes.push_back(envelope2);
-  envelopes.push_back(envelope3);
-  envelopes.push_back(envelope4);
-  envelopes.push_back(envelope5);
-  envelopes.push_back(envelope6);
-  for (auto it = envelopes.begin(); it < envelopes.end(); ++it)
+
+  for (unsigned int i = 0; i < 6; ++i)
   {
-    it->sustain(0.5);
+    oscillators[i]->begin(1.0, 440.0, WAVEFORM_TRIANGLE);
   }
+  
   for (unsigned int i = 0; i < sizeof(note_pins) / sizeof(int); ++i)
   {
     pinMode(note_pins[i], INPUT);
@@ -146,19 +193,15 @@ void loop()
     switch (note_pins[i])
     {
       case 27:
-        freq = notes[29];
         note = 29;
         break;
       case 28:
-        freq = notes[31];
         note = 31;
         break;
       case 29:
-        freq = notes[33];
         note = 33;
         break;
       case 30:
-        freq = notes[36];
         note = 36;
         break;
     }
@@ -166,37 +209,47 @@ void loop()
     {
       if (!playing[note])
       {
-        playing[note] = true;
         attack = pow(analogRead(potentiometers[0]), 3) / pow(1023, 3) * 11800.0;
         release = pow(analogRead(potentiometers[1]), 3) / pow(1023, 3) * 11800.0;
-        for (auto it = envelopes.begin(); it < envelopes.end(); ++it)
+        for (unsigned int i = 0; i < 6; ++i)
         {
-          it->attack(attack);
-          it->release(release);
+          envelopes[i]->attack(attack);
+          envelopes[i]->release(release);
         }
         play_note(note);
       }
     } else if (playing[note])
     {
       stop_note(note);
-      playing[note] = false;
     }
   }
-  oscillator1.frequency(freq);
 }
 
 //Implement polyphony via a circular array in a vector
 void play_note(const unsigned int note)
 {
   Serial.println(String("Playing note ") + note);
-  std::rotate(envelopes.begin(), envelopes.begin() + 1, envelopes.end());
-  AudioEffectEnvelope envelope = envelopes.back();
-  envelope_used[note] = &envelope;
-  envelope.noteOn();
+  unsigned int i = 0;
+  while (i < 6)
+  {
+    if (available[i])
+    {
+      for (unsigned int j = 3 * i; j < 3 * i + 3; ++j)
+      {
+        oscillators[j]->frequency(notes[note]);
+      }
+      playing[note] = i + 1;
+      envelopes[i]->noteOn();
+      available[i] = false;
+      break;
+    }
+    ++i;
+  }
 }
 void stop_note(const unsigned int note)
 {
-  Serial.println(String("Stopping note ") + note);
-  AudioEffectEnvelope * envelope = envelope_used[note];
-  envelope->noteOff();
+  unsigned int envelope = playing[note] - 1;
+  envelopes[envelope]->noteOff();
+  available[envelope] = true;
+  playing[note] = 0;
 }
